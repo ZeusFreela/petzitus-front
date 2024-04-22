2c77722916dae96c9a2b620e16b7e1ab56da1a7a
Maclean <contatolucasmaclean@gmail.com>
2024-04-22 15:07:26 -0300

    feat: Added files to setup initial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Outfit } from 'next/font/google'
 import './globals.css'
+import { Header } from '@/components/organisms/Header/Header'
 
-const inter = Inter({ subsets: ['latin'] })
+const outfit = Outfit({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
 
 export const metadata: Metadata = {
   title: 'Petzitus',
@@ -15,8 +19,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-BR">
+      <body className={outfit.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
