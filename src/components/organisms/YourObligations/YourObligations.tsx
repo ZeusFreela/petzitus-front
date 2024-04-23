c25ddffa1e383243a33f9e3fd158abfa94aae4a5
Maclean <contatolucasmaclean@gmail.com>
2024-04-23 17:35:08 -0300

    feat: Structure, style and responsiveness (1920px x 280px) of the compthird paged page

--- src/components/organisms/YourObligations/YourObligations.tsx
+++ src/components/organisms/YourObligations/YourObligations.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const YourObligations = () => {
+export const YourObligations = () => {
   return (
     <section className="flex w-full flex-col items-start justify-center gap-6 px-24 py-8 md-5:px-20 md-6:px-10 md-7:px-4">
       <h1 className="text-5xl md-5:text-4xl md-7:text-3xl">Suas Obrigações:</h1>
@@ -16,5 +16,3 @@ const YourObligations = () => {
     </section>
   )
 }
-
-export default YourObligations
